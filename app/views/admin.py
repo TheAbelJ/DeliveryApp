ebe50463b8cc5737c61de46f9f80630a4aa3f26a
Abel <abelj636@gmail.com>
2022-02-19 01:34:05 +0530

    Add userview for admin

--- app/views/admin.py
+++ app/views/admin.py
@@ -6,4 +6,9 @@ from flask import jsonify,request,flash,render_template,redirect, url_for
 from flask_login import current_user,LoginManager,login_required,login_user,logout_user
 
 def admin():
-    return render_template("admin.html")
+    if request.method == 'POST':
+        logout_user()
+        return redirect(url_for('userlogin'))
+    else:
+        users=User.objects
+        return render_template("admin.html",users=users)
